89963948e0030ee7e0633ccb7e01bb1cfa4fda12
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2022-10-13 22:19:36 +0530

    Create upload.sh

---
+++ upload.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+cd /tmp
+
+com () 
+{ 
+    tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
+}
+
+time com ccache 1
+
+cd /tmp
+time rclone copy /mnt/tmp/rom/out/target/product/sweet/*.zip remote:sweet -P
+curl -s "https://api.telegram.org/bot1858827137:AAFZVaKOjAhjVyCXfiGgL-SK6dp7_lILZIE/sendMessage" -d chat_id=-509071822 -d text="Build Uploaded Successfully!"
+cd /tmp
+time rclone copy ccache.tar.gz remote:ccache/ci2 -P
