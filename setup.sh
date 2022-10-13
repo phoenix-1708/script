eaf486afd8cf8d6c20f7e373fd68033d04742eb4
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2022-10-13 21:38:17 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-sudo git clone https://github.com/akhilnarang/scripts bscripts && cd bscripts && bash setup/android_build_env.sh && cd ..
-sudo bash kernel_setup.sh
 sudo git config --global user.name "phoenix-1708"
 sudo git config --global user.email "harikumar1708@gmail.com"
 sudo git config --global color.ui true
+sudo apt-get install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+sudo apt-get install -y openjdk-11-jdk
+sudo apt-get install -y wget
+sudo apt-get install -y jq
+sudo git clone https://github.com/akhilnarang/scripts bscripts && cd bscripts && bash setup/android_build_env.sh && cd ..
+sudo bash kernel_setup.sh
+sudo mkdir -p ~/.config/rclone
+sudo cp mnt/tmp/rom/script/rclone.conf ~/.config/rclone/rclone.conf
+df -h && free -h && nproc
+sudo mkdir -p /tmp/ccache
