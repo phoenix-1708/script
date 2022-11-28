ba98f6fb4bdd6ef71d2ef98c39ec39deb97a7707
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2022-11-28 18:11:13 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
-cd ..
-cd os
+cd $(pwd)/tmp
 mkdir rom
 cd rom
 echo "initialising repo"
-sudo repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
+
 echo "Syncing source"
-sudo repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
 echo "cloning device"
-sudo git clone https://github.com/phoenix-1708/android_device_xiaomi_sweet -b arrow-13.0 --depth=1 device/xiaomi/sweet
+git clone https://github.com/phoenix-1708/device_sweet -b main --depth=1 device/xiaomi/sweet
+
 echo "cloning vendor"
-sudo git clone https://github.com/phoenix-1708/vendor_device_xiaomi_sweet -b arrow-13.0 --depth=1 vendor/xiaomi/sweet
-echo "cloning proton-clang"
-sudo git clone https://github.com/kdrag0n/proton-clang -b master --depth=1 prebuilts/clang/host/linux-x86/clang-proton
+git clone https://github.com/phoenix-1708/vendor_sweet -b main --depth=1 vendor/xiaomi/sweet
+
+echo "cloning clang"
+git clone https://gitlab.com/ImSurajxD/clang-r450784d -b master --depth=1 prebuilts/clang/host/linux-x86/clang-r450784d
+git clone https://gitlab.com/nerdprojectorg/SDClang -b 16 --depth=1 prebuilts/clang/host/linux-x86/clang-sdclang
+
+echo "cloning dolby"
+git clone https://gitlab.com/someone5678/vendor_dolby -b thirteen --depth=1 vendor/dolby
+
 echo "cloning kernel"
-sudo git clone https://github.com/olhotorto/kernel_xiaomi_sweet -b dev/13 --depth=1 kernel/xiaomi/sweet
+git clone https://github.com/phoenix-1708/kernel_xiaomi_sm6150 -b t13 --depth=1 kernel/xiaomi/sweet
+
 echo"cloning finished"
-sudo cp kernel/xiaomi/sweet/arch/arm64/configs/sweet_defconfig kernel/xiaomi/sweet/arch/arm64/configs/sweet_user_defconfig
+
+cp kernel/xiaomi/sweet/arch/arm64/configs/sweet_defconfig kernel/xiaomi/sweet/arch/arm64/configs/sweet_user_defconfig
 echo "file copied successfully"
