38c872066cb2363dec0f1fa436403578577a998b
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2022-11-28 18:12:28 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 sudo source build/envsetup.sh
-sudo lunch spark_sweet-user
-export CCACHE_DIR=//os/ccache
+sudo lunch lineage_sweet-userdebug
+export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 sudo ccache -M 12G
 sudo ccache -o compression=true
 sudo ccache -z
-export TZ=Asia/Dhaka
+export BUILD_USERNAME=phoenix1708
+export BUILD_HOSTNAME=prototype
+export TZ=Asia/Jakarta
 sudo mka bacon -j$(nproc --all)
